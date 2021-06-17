8c6ae875556b1a647eb8af2cc06fab98f08dc034
lhkthedevil <linhtutkyawthedevil@gmail.com>
2021-06-17 22:37:18 +0630

    initial commit by lhkthedevil

---
+++ README.md
@@ -0,0 +1,1 @@
+## Devial Template
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './devial/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
